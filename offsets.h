d357abccfd056c156036715971798da994dab54f
s00kin <66147422+s00kin@users.noreply.github.com>
2020-09-22 23:00:11 +0200

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define o_luaRuntime 0x63CF80
+#define o_runFileInternal 0x28A90
+#define o_loadSystemFileInternal 0x289A0
+#define o_loadSystemFile 0x28ED0
+#define o_adhesive 0x49288C
